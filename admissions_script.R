d740e285cd7fc951a3db8a4c66b6cd901c6b8663
Kyle Choi <choikyle96@gmail.com>
2021-03-17 16:55:32 -0400

    missed 3/16, end of 3/17

--- admissions_script.R
+++ admissions_script.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(ggplot2)
 library(corrplot)
+library(caret)
 
 adm <- read_csv("../data/admissions.csv")
 sapply(adm, function(x) sum(is.na(x))) # count # of missing values 
@@ -13,3 +14,13 @@ adm$Research <- as.factor(adm$Research)
 plot(adm) #check for linear relationships between predictors and admission chance percentage
 sub_adm <- adm[, !names(adm) %in% c("Research")] #remove "Research" column to allow for correlation matrix
 corrplot(cor(sub_adm), type = "lower", addCoef.col = "black")
+
+#modeling
+set.seed(22)
+adm_mod <- adm[, !names(adm) %in% c("Serial No.")]
+training <- trainControl(method="cv", number=10)
+
+full_lm <- train(adm_chance ~ ., data=adm_mod, method="lm", trControl=training)
+print(full_lm)
+
+# i want to create regularized models but i realized that some variables are ordinal and if i'm planning on standardizing predictors i need to figure out if standardizing ordinal predictors is acceptable practice.
