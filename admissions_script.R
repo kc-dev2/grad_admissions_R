1d1eeab493793409fb33169fe4407e89efdc6534
Kyle Choi <choikyle96@gmail.com>
2021-03-18 17:19:11 -0400

    end of 3/18

--- admissions_script.R
+++ admissions_script.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(ggplot2)
 library(corrplot)
 library(caret)
+library(glmnet)
 
 adm <- read_csv("../data/admissions.csv")
 sapply(adm, function(x) sum(is.na(x))) # count # of missing values 
@@ -23,4 +24,31 @@ training <- trainControl(method="cv", number=10)
 full_lm <- train(adm_chance ~ ., data=adm_mod, method="lm", trControl=training)
 print(full_lm)
 
+X <- model.matrix(adm_chance ~ ., data=adm_mod)[, -1]
+y <- adm_mod$adm_chance
+lm_ridge <- cv.glmnet(x=X,y=y,alpha=0)
+opt_lam <- lm_ridge$lambda.min
+
+folds <- createFolds(adm$`Serial No.`, k=10)
+res = rep(0,10)
+results <- lapply(folds, function(x) {
+  train <- adm_mod[-x, ]
+  test <- adm_mod[x, ]
+  actual <- test$adm_chance
+  
+  model1 <- lm(adm_chance ~., data = train)
+  pred1 <- predict(model1, test)
+  err1 <- sum(abs(pred1 - actual))
+  
+  X <- model.matrix(adm_chance ~ ., data=train)[, -1]
+  y <- train$adm_chance
+  model2 <- glmnet(x=X,y=y,alpha=0,lambda=opt_lam)
+  pred2 <- predict(model2, test)
+  err2 <- sum(abs(pred2 - actual))
+  
+  return(err2)
+})
+results
+mean(unlist(results))
 # i want to create regularized models but i realized that some variables are ordinal and if i'm planning on standardizing predictors i need to figure out if standardizing ordinal predictors is acceptable practice.
+# Since 
